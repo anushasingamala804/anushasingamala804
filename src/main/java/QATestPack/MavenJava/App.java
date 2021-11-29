790d40f19cfda5b8887b953b8c844a12ddb5282b
Anusha <anusha.credit@gmail.com>
2021-11-29 16:54:18 +0530

    message to commit

---
+++ src/main/java/QATestPack/MavenJava/App.java
@@ -0,0 +1,13 @@
+package QATestPack.MavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/QATestPack/MavenJava/SeleniumTest.java
@@ -0,0 +1,13 @@
+package QATestPack.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+public void browserAuto(){
+	System.out.println("Browserauto");
+}
+public void ChromeAuto(){
+	System.out.println("ChromeAuto");
+}
+}
